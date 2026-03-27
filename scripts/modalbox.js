93c822142fb1cbaa78c2cfaa87daa3b8cb09ee00
Ivan Sivacek <ivan.sivacek@konicaminolta.cz>
2026-03-27 23:26:19 +0100

    arrow update

---
+++ scripts/modalbox.js
@@ -0,0 +1,27 @@
+var slideIndex = 1;
+
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = slides.length; }
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
